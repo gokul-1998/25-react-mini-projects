68062f1d7d0e47288f30d1ac1665afe431973e02
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-02 21:49:26 +0530

    Added till 1.38 completed 5

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load more data";
 
 function App() {
   return (
@@ -18,7 +19,11 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image Slider Component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"}  limit={"10"} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"}  limit={"10"} /> */}
+   
+      {/* Load more products component  */}
+      <LoadMoreData/>
+   
     </div>
   );
 }
