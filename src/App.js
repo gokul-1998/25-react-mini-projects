66d5001ab403ccaed65963e8ce056e262b8855f2
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-02 21:13:47 +0530

    completed till 1.21 proj 4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
     <div className="App">
-     {/* Accordian component */}
-     {/* <Accordian/> */}
+      {/* Accordian component */}
+      {/* <Accordian/> */}
 
-    {/* Random Color component */}
-    {/* <RandomColor/> */}
+      {/* Random Color component */}
+      {/* <RandomColor/> */}
 
-    {/* Star Rating Component */}
-    <StarRating noOfStars={10}/>
+      {/* Star Rating Component */}
+      {/* <StarRating noOfStars={10}/> */}
 
+      {/* Image Slider Component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"}  limit={"10"} />
     </div>
   );
 }
