26d5026385f1c5e9f2d383a2d589cb50fedba415
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-03 07:27:15 +0530

    completed till 2.01:52 proj 6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load more data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -22,8 +24,11 @@ function App() {
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"}  limit={"10"} /> */}
    
       {/* Load more products component  */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
    
+      {/* Tree view component / menu UI / recursive navigation menu */}
+      <TreeView menus={menus}/>
+
     </div>
   );
 }
