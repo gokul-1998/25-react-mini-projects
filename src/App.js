b4e83cfd2e5d89cd2000e4e83759a0a821650e1f
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-03 08:37:56 +0530

    completed scroll-indicator 2.43

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   return (
@@ -36,8 +37,10 @@ function App() {
 
 
       {/* light  and  dark theme switch */}
-      <LightDarkMode /> 
+      {/* <LightDarkMode />  */}
 
+      {/* scroll indicator component */}
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
     </div>
   );
 }
