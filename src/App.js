2e6b0acb77bc5b59c21dc753e3e01b2a6a73349d
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-03 18:23:58 +0530

    added till 2.57 completed custom-tabs

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
 import LightDarkMode from "./components/light-dark-mode";
 import ScrollIndicator from "./components/scroll-indicator";
+import TabTest from "./components/custom-tabs/tab-test";
 
 function App() {
   return (
@@ -40,7 +41,11 @@ function App() {
       {/* <LightDarkMode />  */}
 
       {/* scroll indicator component */}
-      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
+      {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} /> */}
+    
+      {/* custom tabs component */}
+      <TabTest />
+    
     </div>
   );
 }
