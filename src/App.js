383348475bcb4f32debba232b109b32ba7066c62
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-04-03 08:05:23 +0530

    completed light-dark-mode - 2.26

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LoadMoreData from "./components/load more data";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
 import QRCodeGenerator from "./components/qr-code-generator";
+import LightDarkMode from "./components/light-dark-mode";
 
 function App() {
   return (
@@ -31,7 +32,11 @@ function App() {
       {/* <TreeView menus={menus}/> */}
 
       {/* QR code generator */}
-      <QRCodeGenerator/>
+      {/* <QRCodeGenerator/> */}
+
+
+      {/* light  and  dark theme switch */}
+      <LightDarkMode /> 
 
     </div>
   );
